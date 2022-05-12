5164632c7c14d1f94f9741f62bdf294992223b1e
jahanara khatun <zarakhatun58@gmail.com>
2022-05-12 10:03:53 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h6> My Quiz App</h6>
+    </div>
+  );
+}
+
+export default App;
+//jasdsjfsdf
